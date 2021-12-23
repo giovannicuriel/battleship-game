e6c18cf9a3abad90234abbaf6e5209d752f9d85d
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2021-12-23 15:39:13 -0300

    tests: adding another unit test

---
+++ tests/event-broker/subscription-handler-factory-tests.cpp
@@ -0,0 +1,15 @@
+#include "../src/event-broker/subscription-handler-factory.hpp"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::Return;
+using ::testing::_;
+
+
+TEST(SubscriptionHandlerFactoryTest, SuccessfullyBuildAHandler) {
+    SubscriptionHandlerFactoryImpl factory;
+    auto result = factory.build();
+
+    EXPECT_EQ(result, nullptr);
+}
